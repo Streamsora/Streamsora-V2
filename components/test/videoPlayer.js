d4d3c7d4787e166ac9dcd50122598269ca908b1c
Factiven <dabinaya.fathan@gmail.com>
2023-07-22 13:06:56 +0700

    initial commit
    > Development branch for w2g

---
+++ components/test/videoPlayer.js
@@ -0,0 +1,301 @@
+import Player from "../../lib/Artplayer";
+import { useEffect, useState } from "react";
+import { useAniList } from "../../lib/anilist/useAnilist";
+import artplayerPluginHlsQuality from "artplayer-plugin-hls-quality";
+
+const fontSize = [
+  {
+    html: "Small",
+    size: "16px",
+  },
+  {
+    html: "Medium",
+    size: "36px",
+  },
+  {
+    html: "Large",
+    size: "56px",
+  },
+];
+
+export default function VideoPlayerTest({
+  data,
+  id,
+  progress,
+  session,
+  aniId,
+  stats,
+  skip,
+  title,
+  poster,
+  proxy,
+  provider,
+  socket,
+  isPlay,
+  watchData,
+}) {
+  const [url, setUrl] = useState(
+    "https://www013.vipanicdn.net/streamhls/a6fc173913a4b4f5637e802466cda83b/ep.11.1677609121.1080.m3u8"
+  );
+  const [source, setSource] = useState([]);
+  const { markProgress } = useAniList(session);
+
+  const [resolution, setResolution] = useState("auto");
+  const [subSize, setSubSize] = useState({ size: "16px", html: "Small" });
+  const [defSize, setDefSize] = useState();
+  const [subtitle, setSubtitle] = useState();
+  const [defSub, setDefSub] = useState();
+
+  useEffect(() => {
+    const resol = localStorage.getItem("quality");
+    const sub = JSON.parse(localStorage.getItem("subSize"));
+    if (resol) {
+      setResolution(resol);
+    }
+
+    if (provider === "zoro") {
+      const size = fontSize.map((i) => {
+        const isDefault = !sub ? i.html === "Small" : i.html === sub?.html;
+        return {
+          ...(isDefault && { default: true }),
+          html: i.html,
+          size: i.size,
+        };
+      });
+
+      const defSize = size?.find((i) => i?.default === true);
+      setDefSize(defSize);
+      setSubSize(size);
+    }
+
+    async function compiler() {
+      try {
+        const referer = data?.headers?.Referer;
+        const source = data?.sources?.map((items) => {
+          const isDefault =
+            provider !== "gogoanime"
+              ? items.quality === "default" || items.quality === "auto"
+              : resolution === "auto"
+              ? items.quality === "default" || items.quality === "auto"
+              : items.quality === resolution;
+          return {
+            ...(isDefault && { default: true }),
+            html: items.quality === "default" ? "adaptive" : items.quality,
+            url:
+              provider === "gogoanime"
+                ? `https://cors.moopa.my.id/?url=${encodeURIComponent(
+                    items.url
+                  )}${referer ? `&referer=${encodeURIComponent(referer)}` : ""}`
+                : `${proxy}${items.url}`,
+          };
+        });
+
+        const defSource = source?.find((i) => i?.default === true);
+
+        if (defSource) {
+          setUrl(defSource.url);
+        }
+
+        if (provider === "zoro") {
+          const subtitle = data?.subtitles
+            .filter((subtitle) => subtitle.lang !== "Thumbnails")
+            .map((subtitle) => {
+              const isEnglish = subtitle.lang === "English";
+              return {
+                ...(isEnglish && { default: true }),
+                url: subtitle.url,
+                html: `${subtitle.lang}`,
+              };
+            });
+
+          const defSub = data?.subtitles.find((i) => i.lang === "English");
+
+          setDefSub(defSub?.url);
+
+          setSubtitle(subtitle);
+        }
+
+        setSource(source);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    compiler();
+  }, [data, resolution]);
+
+  return (
+    <>
+      {url && (
+        <Player
+          key={url}
+          socket={socket}
+          isPlay={isPlay}
+          watchData={watchData}
+          option={{
+            // url: `${url}`,
+            url: `https://cors.moopa.live/https://www031.vipanicdn.net/streamhls/a6fc173913a4b4f5637e802466cda83b/ep.9.1677821362.1080.m3u8`,
+            title: `${title}`,
+            // autoplay: true,
+            screenshot: true,
+            moreVideoAttr: {
+              crossOrigin: "anonymous",
+            },
+            poster: poster ? poster : "",
+            ...(provider !== "gogoanime" && {
+              plugins: [
+                artplayerPluginHlsQuality({
+                  // Show quality in control
+                  // control: true,
+
+                  // Show quality in setting
+                  setting: true,
+
+                  // Get the resolution text from level
+                  getResolution: (level) => level.height + "P",
+
+                  // I18n
+                  title: "Quality",
+                  auto: "Auto",
+                }),
+              ],
+            }),
+            ...(provider === "zoro" && {
+              subtitle: {
+                url: `${defSub}`,
+                // type: "vtt",
+                encoding: "utf-8",
+                default: true,
+                name: "English",
+                escape: false,
+                style: {
+                  color: "#FFFF",
+                  fontSize: `${defSize?.size}`,
+                  fontFamily: localStorage.getItem("font")
+                    ? localStorage.getItem("font")
+                    : "Arial",
+                  textShadow: localStorage.getItem("subShadow")
+                    ? JSON.parse(localStorage.getItem("subShadow")).value
+                    : "0px 0px 10px #000000",
+                },
+              },
+            }),
+          }}
+          res={resolution}
+          quality={source}
+          subSize={subSize}
+          subtitles={subtitle}
+          provider={provider}
+          style={{
+            width: "100%",
+            height: "100%",
+            margin: "0 auto 0",
+          }}
+          getInstance={(art) => {
+            art.on("ready", () => {
+              // console.log(art.storage.settings);
+              const seek = art.storage.get(id);
+              const seekTime = seek?.time || 0;
+              const duration = art.duration;
+              const percentage = seekTime / duration;
+
+              if (subSize) {
+                art.subtitle.style.fontSize = subSize?.size;
+              }
+
+              if (percentage >= 0.9) {
+                art.currentTime = 0;
+                console.log("Video started from the beginning");
+              } else {
+                art.currentTime = seekTime;
+              }
+            });
+
+            art.on("video:timeupdate", () => {
+              if (!session) return;
+              const mediaSession = navigator.mediaSession;
+              const currentTime = art.currentTime;
+              const duration = art.duration;
+              const percentage = currentTime / duration;
+
+              mediaSession.setPositionState({
+                duration: art.duration,
+                playbackRate: art.playbackRate,
+                position: art.currentTime,
+              });
+
+              if (percentage >= 0.9) {
+                // use >= instead of >
+                markProgress(aniId, progress, stats);
+                art.off("video:timeupdate");
+                console.log("Video progress marked");
+              }
+            });
+
+            art.on("video:timeupdate", () => {
+              var currentTime = art.currentTime;
+              // console.log(art.currentTime);
+              art.storage.set(id, {
+                time: art.currentTime,
+                duration: art.duration,
+              });
+
+              if (
+                skip?.op &&
+                currentTime >= skip.op.interval.startTime &&
+                currentTime <= skip.op.interval.endTime
+              ) {
+                // Add the layer if it's not already added
+                if (!art.controls["op"]) {
+                  // Remove the other control if it's already added
+                  if (art.controls["ed"]) {
+                    art.controls.remove("ed");
+                  }
+
+                  // Add the control
+                  art.controls.add({
+                    name: "op",
+                    position: "top",
+                    html: '<button class="skip-button">Skip Opening</button>',
+                    click: function (...args) {
+                      art.seek = skip.op.interval.endTime;
+                    },
+                  });
+                }
+              } else if (
+                skip?.ed &&
+                currentTime >= skip.ed.interval.startTime &&
+                currentTime <= skip.ed.interval.endTime
+              ) {
+                // Add the layer if it's not already added
+                if (!art.controls["ed"]) {
+                  // Remove the other control if it's already added
+                  if (art.controls["op"]) {
+                    art.controls.remove("op");
+                  }
+
+                  // Add the control
+                  art.controls.add({
+                    name: "ed",
+                    position: "top",
+                    html: '<button class="skip-button">Skip Ending</button>',
+                    click: function (...args) {
+                      art.seek = skip.ed.interval.endTime;
+                    },
+                  });
+                }
+              } else {
+                // Remove the controls if they're added
+                if (art.controls["op"]) {
+                  art.controls.remove("op");
+                }
+                if (art.controls["ed"]) {
+                  art.controls.remove("ed");
+                }
+              }
+            });
+          }}
+        />
+      )}
+    </>
+  );
+}
--- pages/en/test.js
+++ pages/en/test.js
@@ -1,10 +1,78 @@
-import VideoPlayer from "../../components/videoPlayer";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+import VideoPlayerTest from "../../components/test/videoPlayer";
+
+const socket = io.connect("http://localhost:3002");
 
 export default function Test() {
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [messageRecieved, setMessageRecieved] = useState([]);
+  const [watchData, setWatchData] = useState({
+    isPlay: false,
+    room: "",
+    time: null,
+  });
+  const [isPlay, setIsPlay] = useState(false);
+
+  console.log(isPlay);
+
+  const sendMessage = () => {
+    socket.emit("chat message", message);
+  };
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
+  console.log(watchData);
+
+  useEffect(() => {
+    socket.on("recieved_message", (data) => {
+      let dataRecieved = [...messageRecieved];
+      dataRecieved.push(data);
+
+      setMessageRecieved(dataRecieved);
+    });
+
+    socket.on("play_video", (data) => {
+      // console.log();
+      const get = JSON.parse(data);
+      setWatchData(get);
+      setIsPlay(true);
+    });
+
+    socket.on("pause_video", (data) => {
+      const get = JSON.parse(data);
+      setWatchData(get);
+      setIsPlay(false);
+    });
+
+    socket.on("user_joined", (data) => {
+      alert("User joined room: " + data);
+    });
+  }, [socket]);
+
+  // console.log(messageRecieved);
+
   return (
-    <div className="flex-center w-screen h-screen">
-      <div className="h-full aspect-video bg-black">
-        <VideoPlayer />
+    <div className="flex-center flex-col w-screen h-screen">
+      <div className="w-[30%] flex gap-5">
+        <input
+          placeholder="Type message..."
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <button onClick={joinRoom}>Send Message</button>
+        <p>Message recieved: {messageRecieved}</p>
+      </div>
+      <div className="h-auto w-screen aspect-video bg-black">
+        <VideoPlayerTest
+          socket={socket}
+          isPlay={isPlay}
+          watchData={watchData}
+        />
       </div>
     </div>
   );
