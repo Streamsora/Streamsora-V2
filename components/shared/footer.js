5f863aabf8576cde086b468c999511a36608f659
araxyso <69326871+Zinedinarnaut@users.noreply.github.com>
2023-09-27 23:38:59 +0300

    Update footer.js

--- components/shared/footer.js
+++ components/shared/footer.js
@@ -24,8 +24,8 @@ function Footer() {
     if (lang === "en" || lang === null) {
       setLang("en");
       setChecked(false);
-    } else if (lang === "id") {
-      setLang("id");
+    } else if (lang === "jp") {
+      setLang("jp");
       setChecked(true);
     }
   }, []);
@@ -40,7 +40,7 @@ function Footer() {
       });
       router.push("/en");
     } else {
-      router.push("/id");
+      router.push("/jp");
     }
   }
 
@@ -57,7 +57,7 @@ function Footer() {
                 height={100}
                 className="w-10 h-10"
               /> */}
-            <div className="flex gap-2 font-outfit text-4xl">moopa</div>
+            <div className="flex gap-2 font-outfit text-4xl">Streamsora</div>
             <p className="font-karla lg:text-[0.8rem] text-[0.65rem] text-[#9c9c9c]  lg:w-[520px] italic">
               This site does not store any files on our server, we only linked
               to the media which is hosted on 3rd party services.
@@ -80,9 +80,6 @@ function Footer() {
                 <li className="cursor-pointer hover:text-action">
                   <Link href={`/${lang}/search/manga`}>Popular Manga</Link>
                 </li>
-                <li className="cursor-pointer hover:text-action">
-                  <Link href={`/donate`}>Donate</Link>
-                </li>
               </ul>
               <ul className="flex flex-col gap-y-[0.7rem]">
                 <li className="cursor-pointer hover:text-action">
@@ -97,9 +94,6 @@ function Footer() {
                   <Link href={`/${lang}/dmca`}>DMCA</Link>
                 </li>
                 <li className="cursor-pointer hover:text-action">
-                  <Link href="https://github.com/DevanAbinaya/Ani-Moopa">
-                    Github
-                  </Link>
                 </li>
               </ul>
             </div>
@@ -109,13 +103,13 @@ function Footer() {
       <div className="bg-tersier border-t border-white/5">
         <div className="mx-auto flex w-[90%] lg:w-[95%] xl:w-[80%] flex-col pb-6 lg:flex-row lg:items-center lg:justify-between lg:space-y-0 lg:py-0">
           <p className="flex items-center gap-1 font-karla lg:text-[0.81rem] text-[0.7rem] text-[#CCCCCC] py-3">
-            &copy; {new Date().getFullYear()} moopa.live | Website Made by{" "}
-            <span className="text-white font-bold">Factiven</span>
+            &copy; {new Date().getFullYear()} streamsora.live | Website designed by{" "}
+            <span className="text-white font-bold">Factiven | Hosted By Streamsora</span>
           </p>
           <div className="flex items-center gap-5">
             {/* Github Icon */}
             <Link
-              href="https://github.com/Ani-Moopa/Moopa"
+              href="https://github.com/streamsora/"
               className="w-5 h-5 hover:opacity-75"
             >
               <svg
@@ -141,7 +135,7 @@ function Footer() {
             </Link>
             {/* Discord Icon */}
             <Link
-              href="https://discord.gg/v5fjSdKwr2"
+              href="https://discord.gg/"
               className="w-6 h-6 hover:opacity-75"
             >
               <svg
@@ -153,17 +147,6 @@ function Footer() {
                   fill="#fff"
                   d="M216.856 16.597A208.502 208.502 0 00164.042 0c-2.275 4.113-4.933 9.645-6.766 14.046-19.692-2.961-39.203-2.961-58.533 0-1.832-4.4-4.55-9.933-6.846-14.046a207.809 207.809 0 00-52.855 16.638C5.618 67.147-3.443 116.4 1.087 164.956c22.169 16.555 43.653 26.612 64.775 33.193A161.094 161.094 0 0079.735 175.3a136.413 136.413 0 01-21.846-10.632 108.636 108.636 0 005.356-4.237c42.122 19.702 87.89 19.702 129.51 0a131.66 131.66 0 005.355 4.237 136.07 136.07 0 01-21.886 10.653c4.006 8.02 8.638 15.67 13.873 22.848 21.142-6.58 42.646-16.637 64.815-33.213 5.316-56.288-9.08-105.09-38.056-148.36zM85.474 135.095c-12.645 0-23.015-11.805-23.015-26.18s10.149-26.2 23.015-26.2c12.867 0 23.236 11.804 23.015 26.2.02 14.375-10.148 26.18-23.015 26.18zm85.051 0c-12.645 0-23.014-11.805-23.014-26.18s10.148-26.2 23.014-26.2c12.867 0 23.236 11.804 23.015 26.2 0 14.375-10.148 26.18-23.015 26.18z"
                 ></path>
-              </svg>
-            </Link>
-
-            {/* Kofi */}
-            <Link href="/donate" className="w-6 h-6 hover:opacity-75">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#fff"
-                viewBox="0 0 24 24"
-              >
-                <path d="M23.881 8.948c-.773-4.085-4.859-4.593-4.859-4.593H.723c-.604 0-.679.798-.679.798s-.082 7.324-.022 11.822c.164 2.424 2.586 2.672 2.586 2.672s8.267-.023 11.966-.049c2.438-.426 2.683-2.566 2.658-3.734 4.352.24 7.422-2.831 6.649-6.916zm-11.062 3.511c-1.246 1.453-4.011 3.976-4.011 3.976s-.121.119-.31.023c-.076-.057-.108-.09-.108-.09-.443-.441-3.368-3.049-4.034-3.954-.709-.965-1.041-2.7-.091-3.71.951-1.01 3.005-1.086 4.363.407 0 0 1.565-1.782 3.468-.963 1.904.82 1.832 3.011.723 4.311zm6.173.478c-.928.116-1.682.028-1.682.028V7.284h1.77s1.971.551 1.971 2.638c0 1.913-.985 2.667-2.059 3.015z"></path>
               </svg>
             </Link>
 
@@ -183,7 +166,7 @@ function Footer() {
               </span>
               <span className="absolute font-medium text-xs uppercase right-[2.1rem] text-action">
                 {" "}
-                ID{" "}
+                JP{" "}
               </span>
               <span className="w-6 h-6 right-[2.1rem] absolute rounded-full transform transition-transform bg-gray-200" />
             </label>
