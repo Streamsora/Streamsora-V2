62e68c0b53ef134e07b6855c9970dab0f3408eb6
zinedinarnaut <zinedinarnaut085@gmail.com>
2023-10-23 07:12:04 +1100

    Update

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,40 +1,91 @@
-import axios from "axios";
+import AdminDashboard from "@/components/admin/dashboard";
+import AdminLayout from "@/components/admin/layout";
+import AppendMeta from "@/components/admin/meta/AppendMeta";
+import {
+  countKeysWithPrefix,
+  countNumericKeys,
+  getValuesWithPrefix,
+} from "@/utils/getRedisWithPrefix";
+import { getServerSession } from "next-auth";
+import { authOptions } from "pages/api/auth/[...nextauth]";
+import React, { useState } from "react";
 
-export async function fetchInfo(romaji, english, native) {
-  try {
-    const { data: getManga } = await axios.get(
-      `https://api.anify.tv/search-advanced?query=${
-        english || romaji
-      }&type=manga`
-    );
+export async function getServerSideProps(context) {
+  const sessions = await getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
 
-    const findManga = getManga.find(
-      (manga) =>
-        manga.title.romaji === romaji ||
-        manga.title.english === english ||
-        manga.title.native === native
-    );
-
-    if (!findManga) {
-      return null;
-    }
-
-    return { id: findManga.id };
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return null;
+  if (!sessions) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
   }
+
+  const admin = sessions?.user?.name === process.env.ADMIN_USERNAME;
+
+  let api;
+  api = process.env.API_URI;
+  if (api.endsWith("/")) {
+    api = api.slice(0, -1);
+  }
+
+  if (!admin) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const [anime, info, meta, report] = await Promise.all([
+    countNumericKeys(),
+    countKeysWithPrefix("anime:"),
+    countKeysWithPrefix("meta:"),
+    getValuesWithPrefix("report:"),
+  ]);
+
+  return {
+    props: {
+      session: sessions,
+      animeCount: anime || 0,
+      infoCount: info || 0,
+      metaCount: meta || 0,
+      report: report || [],
+      api,
+    },
+  };
 }
 
-export default async function getMangaId(romaji, english, native) {
-  try {
-    const data = await fetchInfo(romaji, english, native);
-    if (data) {
-      return data;
-    } else {
-      return { message: "Schedule not found" };
-    }
-  } catch (error) {
-    return { error };
-  }
+export default function Admin({
+                                animeCount,
+                                infoCount,
+                                metaCount,
+                                report,
+                                api,
+                              }) {
+  const [page, setPage] = useState(1);
+
+  return (
+    <AdminLayout page={page} setPage={setPage}>
+      <div className="h-full">
+        {page == 1 && (
+          <AdminDashboard
+            animeCount={animeCount}
+            infoCount={infoCount}
+            metaCount={metaCount}
+            report={report}
+          />
+        )}
+        {page == 2 && <AppendMeta api={api} />}
+        {page == 3 && <p className="flex-center h-full">Coming Soon!</p>}
+        {page == 4 && <p className="flex-center h-full">Coming Soon!</p>}
+      </div>
+    </AdminLayout>
+  );
 }
