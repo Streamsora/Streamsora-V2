2ca175dc30df3bb3f0279f3742eaee6c3910c913
zined <zinedinarnaut085@gmail.com>
2023-12-23 22:19:31 +1100

    Update

--- components/Modals/ChangeLogs.js
+++ components/Modals/ChangeLogs.js
@@ -49,21 +49,8 @@ export default function ChangelogModal({ isOpen, setIsOpen }) {
                     <p className="text-xl">Changelogs</p>
                     <div className="flex gap-2 items-center">
                       <a
-                        className="w-5 h-5 hover:opacity-75"
-                        href="https://hypertabs.zt0ht.repl.co/uv/service/hvtrs8%2F-mmora%2Clkvg%2Feivhwb"
-                      >
-                        {/* GitHub SVG */}
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="#fff"
-                          viewBox="0 0 20 20"
-                        >
-                          {/* ... GitHub SVG content */}
-                        </svg>
-                      </a>
-                      <a
                         className="w-6 h-6 hover:opacity-75"
-                        href="https://hypertabs.zt0ht.repl.co/uv/service/hvtrs8%2F-mmora%2Clkvg%2Ffiqcmrf"
+                        href="https://discord.gg/B8YV3nFnDa"
                       >
                         {/* Discord SVG */}
                         <svg
@@ -71,7 +58,7 @@ export default function ChangelogModal({ isOpen, setIsOpen }) {
                           preserveAspectRatio="xMidYMid"
                           viewBox="0 -28.5 256 256"
                         >
-                          {/* ... Discord SVG content */}
+                          <path fill="#fff" d="M216.856 16.597A208.502 208.502 0 00164.042 0c-2.275 4.113-4.933 9.645-6.766 14.046-19.692-2.961-39.203-2.961-58.533 0-1.832-4.4-4.55-9.933-6.846-14.046a207.809 207.809 0 00-52.855 16.638C5.618 67.147-3.443 116.4 1.087 164.956c22.169 16.555 43.653 26.612 64.775 33.193A161.094 161.094 0 0079.735 175.3a136.413 136.413 0 01-21.846-10.632 108.636 108.636 0 005.356-4.237c42.122 19.702 87.89 19.702 129.51 0a131.66 131.66 0 005.355 4.237 136.07 136.07 0 01-21.886 10.653c4.006 8.02 8.638 15.67 13.873 22.848 21.142-6.58 42.646-16.637 64.815-33.213 5.316-56.288-9.08-105.09-38.056-148.36zM85.474 135.095c-12.645 0-23.015-11.805-23.015-26.18s10.149-26.2 23.015-26.2c12.867 0 23.236 11.804 23.015 26.2.02 14.375-10.148 26.18-23.015 26.18zm85.051 0c-12.645 0-23.014-11.805-23.014-26.18s10.148-26.2 23.014-26.2c12.867 0 23.236 11.804 23.015 26.2 0 14.375-10.148 26.18-23.015 26.18z"></path>
                         </svg>
                       </a>
                     </div>
@@ -79,7 +66,7 @@ export default function ChangelogModal({ isOpen, setIsOpen }) {
                 </h3>
                 <div className="mt-4">
                   <p className="text-sm text-gray-400">
-                    Hi! Welcome to the new changelogs section. Here you can see
+                    Hey! Welcome to the new changelogs section. Here you can see
                     a lists of the latest changes and updates to the site.
                   </p>
                   <p className="inline-block text-sm italic my-2 text-gray-400">
@@ -122,8 +109,8 @@ export default function ChangelogModal({ isOpen, setIsOpen }) {
                 <div className="flex flex-col gap-2 text-sm py-2 text-gray-200">
                   <div>
                     <p>- Added changelogs section</p>
-                    <p>- Added recommendations based on user lists</p>
-                    <p>- New Player!</p>
+                    <p>- Added hanime section</p>
+                    <p>- Added notifications section (Coming Soon)</p>
                     <p>- And other minor bug fixes!</p>
                   </div>
                 </div>
