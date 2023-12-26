cec2c56e8ce3fb6e1b3d0abc3ebeb433baa6d8fd
zined <zinedinarnaut085@gmail.com>
2023-12-27 09:49:34 +1100

    Update

--- pages/en/stats.js
+++ pages/en/stats.js
@@ -8,6 +8,7 @@ import { FaVideo, FaBook, FaRegClock, FaKey } from "react-icons/fa"; // Import i
 
 export default function Stats() {
   const [apiData, setApiData] = useState(null);
+/*  const [apiData1, setApiData1] = useState(null);*/
 
   useEffect(() => {
     fetch("https://api.anify.tv/stats")
@@ -15,6 +16,12 @@ export default function Stats() {
       .then((data) => setApiData(data))
       .catch((error) => console.error("Error fetching API data:", error));
   }, []);
+/*  useEffect(() => {
+    fetch("https://scrape.streamsora.live/stats")
+      .then((response) => response.json())
+      .then((data1) => setApiData1(data1))
+      .catch((error) => console.error("Error fetching API data:", error));
+  }, []);*/
 
   const pageVariants = {
     initial: {
@@ -35,6 +42,7 @@ export default function Stats() {
       transition={{ duration: 1 }}
       className="min-h-screen flex items-center justify-center text-white"
     >
+
       <Head>
         <title>Streamsora - Stats</title>
         <meta name="Stats" content="Stats" />
@@ -43,13 +51,14 @@ export default function Stats() {
         <NewNavbar withNav={true} scrollP={5} shrink={true} />
 
         <MobileNav hideProfile={true} />
+        {/* First Table (Production Stats) */}
         <motion.div
           className="w-full max-w-screen-lg p-8 rounded-lg bg-secondary shadow-lg"
-          initial={{ scale: 0.8, opacity: 0 }} // Initial scale and opacity
+          initial={{ scale: 0.8, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.5 }}
         >
-          <h1 className="text-4xl font-bold text-center mb-6">Stats</h1>
+          <h1 className="text-4xl font-bold text-center mb-6">Production Stats</h1>
           {apiData && (
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 text-[#ccc]">
               <div className="stat-box p-4 border border-gray-700 rounded-lg flex items-center">
@@ -80,6 +89,44 @@ export default function Stats() {
             </div>
           )}
         </motion.div>
+{/*         Second Table (Development Stats)
+        <motion.div
+          className="w-full max-w-screen-lg p-8 rounded-lg bg-secondary shadow-lg"
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
+          <h1 className="text-4xl font-bold text-center mb-6">Development Stats</h1>
+          {apiData1 && (
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 text-[#ccc]">
+              <div className="stat-box p-4 border border-gray-700 rounded-lg flex items-center">
+                <FaVideo className="stat-icon mr-2 text-blue-500" />
+                <p className="text-lg">Anime</p>
+                <p className="text-2xl font-semibold ml-auto">{apiData1.anime}</p>
+              </div>
+              <div className="stat-box p-4 border border-gray-700 rounded-lg flex items-center">
+                <FaBook className="stat-icon mr-2 text-green-500" />
+                <p className="text-lg">Manga</p>
+                <p className="text-2xl font-semibold ml-auto">{apiData1.manga}</p>
+              </div>
+              <div className="stat-box p-4 border border-gray-700 rounded-lg flex items-center">
+                <FaBook className="stat-icon mr-2 text-red-500" />
+                <p className="text-lg">Novels</p>
+                <p className="text-2xl font-semibold ml-auto">{apiData1.novels}</p>
+              </div>
+              <div className="stat-box p-4 border border-gray-700 rounded-lg flex items-center">
+                <FaRegClock className="stat-icon mr-2 text-purple-500" />
+                <p className="text-lg">Skip Times</p>
+                <p className="text-2xl font-semibold ml-auto">{apiData1.skipTimes}</p>
+              </div>
+              <div className="stat-box p-4 border border-gray-700 rounded-lg flex items-center">
+                <FaKey className="stat-icon mr-2 text-orange-500" />
+                <p className="text-lg">API Keys</p>
+                <p className="text-2xl font-semibold ml-auto">{apiData1.apiKeys}</p>
+              </div>
+            </div>
+          )}*/}
+        {/*</motion.div>*/}
       </>
     </motion.div>
   );
